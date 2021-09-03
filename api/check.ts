215db882f12ca1c5dc8e5b6be91173b3a59a569d
Wes Baker <wes@wesbaker.com>
2021-09-03 10:23:46 -0400

    Update to using VercelRequest/Response

--- api/check.ts
+++ api/check.ts
@@ -8,7 +8,7 @@ import Raven from "raven";
 import tweet from "../lib/tweet";
 import "../models/Post";
 import { ResponseData } from "twitter";
-import { NowRequest, NowResponse } from "@vercel/node";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
 dotenv.config();
 Raven.config(process.env.SENTRY_DSN).install();
@@ -29,7 +29,10 @@ function logError(error: Error): void {
   console.error(error);
 }
 
-export default async (req: NowRequest, res: NowResponse): Promise<void> => {
+export default async (
+  req: VercelRequest,
+  res: VercelResponse
+): Promise<void> => {
   const feed = await parser
     .parseURL("https://www.enworld.org/ewr-porta/index.rss")
     .catch(logError);
